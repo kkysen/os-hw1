7cbec70bda7aecf57101e8c3c4ea24162082216c
Khyber Sen <kkysen@gmail.com>
2021-09-23 04:12:00 -0400

    Finished part4.  Pretty simple once I realized I have to iterate over the siblings (not children) of the children list.

--- part4/tasklist.c
+++ part4/tasklist.c
@@ -1,9 +1,31 @@
 #include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/sched/task.h>
+
+void print_process(const struct task_struct *const process, const u32 level)
+{
+	const bool is_root = level == 0;
+	/* don't indent too far, and don't put an arrow on root/init */
+	const char *const arrow = is_root ? "" : " \\_ ";
+	const u32 indents = min(is_root ? 0 : level - 1, (u32)20);
+	const u32 num_spaces = indents * 4;
+	printk(KERN_INFO "%*s%s[%d] %s\n", num_spaces, "", arrow, process->pid,
+	       process->comm);
+}
+
+void print_process_tree(const struct task_struct *const parent, const u32 level)
+{
+	const struct task_struct *child;
+	print_process(parent, level);
+	list_for_each_entry (child, &parent->children, sibling) {
+		print_process_tree(child, level + 1);
+	}
+}
 
 int tasklist_init(void)
 {
 	printk(KERN_INFO "Loading Module\n");
-
+	print_process_tree(&init_task, 0);
 	return 0;
 }
 
